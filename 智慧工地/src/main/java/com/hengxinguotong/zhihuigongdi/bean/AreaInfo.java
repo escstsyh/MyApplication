13d07b077db5f4d3a8843488de941585412db508
escstsyh <591551784@qq.com>
2016-10-26 11:38:51 +0800

    ww

---
+++ 智慧工地/src/main/java/com/hengxinguotong/zhihuigongdi/bean/AreaInfo.java
@@ -0,0 +1,21 @@
+package com.hengxinguotong.zhihuigongdi.bean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/9/21.
+ */
+public class AreaInfo {
+
+
+    public String message;
+    public String result;
+    public List<Area> data;
+
+    public class Area {
+        public String areaCode;//区域码
+        public String territoryname;//区域名
+
+
+    }
+}
